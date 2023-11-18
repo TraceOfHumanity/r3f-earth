43dcc4e4ad614beb4cdcaf4eaf0d4e1a553099ae
Alexander_Ovchar <azalexxx02@gmail.com>
2023-11-18 05:42:54 +0200

    add moon

--- src/components/Earth/index.jsx
+++ src/components/Earth/index.jsx
@@ -8,28 +8,38 @@ import EarthDayMap from "../../assets/textures/8k_earth_daymap.jpg";
 import EarthNormalMap from "../../assets/textures/8k_earth_normal_map.jpg";
 import EarthSpecularMap from "../../assets/textures/8k_earth_specular_map.jpg";
 import EarthCloudsMap from "../../assets/textures/8k_earth_clouds.jpg";
+import MoonMap from "../../assets/textures/moon.jpg";
 import { TextureLoader } from "three";
 
 export const Earth = (props) => {
-  const [dayMap, normalMap, specularMap, cloudsMap] = useLoader(TextureLoader, [
-    EarthDayMap,
-    EarthNormalMap,
-    EarthSpecularMap,
-    EarthCloudsMap,
-  ]);
+  const [dayMap, normalMap, specularMap, cloudsMap, moonMap] = useLoader(
+    TextureLoader,
+    [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap, MoonMap]
+  );
 
   const earthRef = React.useRef();
   const cloudsRef = React.useRef();
+  const sunRef = React.useRef();
+  const moonRef = React.useRef();
   useFrame(({ clock }) => {
     const elapsedTime = clock.getElapsedTime();
     earthRef.current.rotation.y = elapsedTime / 60;
     cloudsRef.current.rotation.y = elapsedTime / 70;
+
+    sunRef.current.position.x = Math.cos(elapsedTime / 30) * 20;
+    sunRef.current.position.z = Math.sin(elapsedTime / 30) * 20;
+    sunRef.current.position.y = Math.sin(elapsedTime / -30) * 10;
+
+    moonRef.current.position.x = Math.cos(elapsedTime / 40) * 3;
+    moonRef.current.position.z = Math.sin(elapsedTime / 40) * 3;
+
+    moonRef.current.rotation.y = elapsedTime / 50;
   });
 
   return (
     <>
       {/* <ambientLight intensity={1} /> */}
-      <pointLight position={[-2, 1, 2.7]} intensity={20} />
+      {/* <pointLight position={[-2, 1, 2.7]} intensity={20} /> */}
       <Stars
         radius={300}
         depth={60}
@@ -38,7 +48,7 @@ export const Earth = (props) => {
         saturation={0}
         fade={true}
       />
-      <mesh ref={cloudsRef} position={[0, 0, 1.5]}>
+      <mesh ref={cloudsRef} position={[0, 0, 0]}>
         <sphereGeometry args={[1.006, 32, 32]} />
         <meshPhongMaterial
           map={cloudsMap}
@@ -48,8 +58,8 @@ export const Earth = (props) => {
           side={THREE.DoubleSide}
         />
       </mesh>
-      <mesh ref={earthRef} position={[0, 0, 1.5]}>
-        <sphereGeometry args={[1, 32, 32]} />
+      <mesh ref={earthRef} position={[0, 0, 0]}>
+        <sphereGeometry args={[1, 100, 100]} />
         <meshPhongMaterial specularMap={specularMap} />
         <meshStandardMaterial
           map={dayMap}
@@ -67,6 +77,16 @@ export const Earth = (props) => {
           panSpeed={0.5}
           rotateSpeed={0.4}
         /> */}
+      </mesh>
+      <mesh position={[3, 0, 1]} rotation={[0, 0, 0]} ref={moonRef}>
+        <sphereGeometry args={[0.1, 32, 32]} />
+        <meshPhongMaterial map={moonMap} />
+        <pointLight intensity={0.2} />
+      </mesh>
+      <mesh position={[10, 0, -10]} ref={sunRef} rotation={[0, 0, 0]}>
+        <sphereGeometry args={[0.3, 32, 32]} />
+        <meshPhongMaterial emissive="yellow" />
+        <pointLight intensity={2000} />
       </mesh>
     </>
   );
